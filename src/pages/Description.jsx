a9c9d5327a39685e1dc096567bb4e39220900713
Giovanni <giovanni.lor23@gmail.com>
2024-02-03 17:31:11 +0100

    updates

--- src/pages/Description.jsx
+++ src/pages/Description.jsx
@@ -1,51 +1,70 @@
-import { useEffect } from 'react'; 
-import { useSelector, useDispatch} from 'react-redux';
-import { selectDescriptionResults, selectApiKey, setResultsArray, setDescriptionId, setDescriptionResults } from '../redux/slice';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectDescriptionResults, selectApiKey, setDescriptionResults } from '../redux/slice';
 import Navbar from '../components/Navbar';
 import axios from 'axios';
 
-export default function Description(){
-    const results = useSelector(selectDescriptionResults);
-    const apiKey = useSelector(selectApiKey);
-    const dispatch = useDispatch();
+export default function Description() {
+  const results = useSelector(selectDescriptionResults);
+  const apiKey = useSelector(selectApiKey);
+  const dispatch = useDispatch();
 
-    useEffect(()=>{
-        async function fetchDescription() {
-            try {
-              const id = decodeURIComponent(location.pathname.split('/description/')[1]);
-              const response = await axios.get (
-                `https://api.spoonacular.com/recipes/${id}/information`,
-                {
-                  params: {
-                    apiKey: apiKey,
-                  },
-                }
-              );
+  useEffect(() => {
+    async function fetchDescription() {
+      try {
+        const id = decodeURIComponent(location.pathname.split('/description/')[1]);
+        const response = await axios.get(
+          `https://api.spoonacular.com/recipes/${id}/information`,
+          {
+            params: {
+              apiKey: apiKey,
+            },
+          }
+        );
 
-              const results = response.data;
-              dispatch(setDescriptionResults(results))
-            } catch (error) {
-              console.error('Error fetching data:', error.message);
-          }}
 
-        fetchDescription()
-    },[apiKey, dispatch])
-    
+        const results = response.data;
+        dispatch(setDescriptionResults(results))
+        console.log(results)
+      } catch (error) {
+        console.error('Error fetching data:', error.message);
+      }
+    }
 
-    const convertHtmlString = (htmlString) => {
-        return { __html: htmlString };
-      };
+    fetchDescription()
+  }, [apiKey, dispatch])
 
-    return(
-        <div>
-        <Navbar />
-        <div className='description_container'>
+
+  const convertHtmlString = (htmlString) => {
+    return { __html: htmlString };
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className='description_container'>
+        <div className='left_container'>
+          <img src={results.image} alt={results.title} />
+        </div>
+        <div className='right_container'>
+          <div className='title_container'>
             <h1>{results.title}</h1>
-            <img src={results.image} alt={results.title} />
             <p dangerouslySetInnerHTML={convertHtmlString(results.summary)}></p>
-            <br />
-            <p dangerouslySetInnerHTML={convertHtmlString(results.instructions)}></p>
+          </div>
+          <div className='ingredients_container'>
+            <ul>
+              {results.extendedIngredients &&
+                results.extendedIngredients.map((item) => (
+                  <li key={item.id}>{item.aisle && `${item.aisle}:`} {item.original}</li>
+                ))}
+            </ul>
+          </div>
         </div>
-        </div>
-    )
+      </div>
+
+      <br />
+      <p dangerouslySetInnerHTML={convertHtmlString(results.instructions)}></p>
+
+    </div>
+  )
 }
